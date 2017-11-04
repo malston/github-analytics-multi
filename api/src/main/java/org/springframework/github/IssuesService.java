3965274dae302fb04e488a99524272bfd5f77061
Mark Alston <malston@pivotal.io>
2017-11-03 19:58:59 -0600

    Polishing...

--- api/src/main/java/org/springframework/github/IssuesService.java
+++ api/src/main/java/org/springframework/github/IssuesService.java
@@ -1,12 +1,12 @@
 package org.springframework.github;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Marcin Grzejszczak
